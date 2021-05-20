a4125298a916c36b7275b63d418c3e9f3b8ad541
Cam Garrison <57603418+cam-garrison@users.noreply.github.com>
2021-05-20 15:56:36 -0400

    Update example_string_code.py

--- example_string_code.py
+++ example_string_code.py
@@ -4,10 +4,11 @@ import string
 # This will create a list of lists of words
 tokens = [sub.split() for sub in big_list]
 
+
 # loop through list of lists
-for wordlist in tokens:
-    for x in range(len(wordlist)):
-        if wordlist[x] == '':
+for i in range(len(tokens)):
+    for x in range(len(tokens[i])):
+        if tokens[i][x] == '':
             continue
-        if wordlist[x] in nameslist:
+        if tokens[i][x] in nameslist:
             wordlist[x] = "name"
